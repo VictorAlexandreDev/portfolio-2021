95a576506b2583972422a0349a5be58203ed3524
Victor Alexandre Borges <Victor17alexandre.borges@gmail.com>
2021-03-27 09:57:48 -0300

    terminei o menu mobile

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,14 @@
 function menu(){
+   let button = document.querySelector('h3>i.fa')
    let menu = document.querySelector('ul.menu-mobile')
    if(menu.classList.contains('esconder')){
+      button.classList.remove('fa-bars')
+      button.classList.add('fa-times')
       menu.classList.remove('esconder')
       menu.classList.add('mostrar')
    }else{
+      button.classList.remove('fa-times')
+      button.classList.add('fa-bars')
       menu.classList.remove('mostrar')
       menu.classList.add('esconder')
    }
